125492260ccd811fc82b5b973ecddf267e2b104a
Denis <boss.karelov@gmail.com>
2023-01-15 20:58:57 +0300

    Заполнение городов и фильтров для дальнейшего использования в поиске

--- main.py
+++ main.py
@@ -7,14 +7,28 @@ from config import *
 import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
-# комменатрий
+
+# Filters
+Cities = []
+Areas = []
+Categories = []
+Sub_Categories = []
+
 driver = uc.Chrome()
 driver.get(website)
 try:
-    filters = driver.find_element(By.CLASS_NAME, 'selected-section')
-    print(filters.get_attribute('innerHTML'))
-    time.sleep(10)
+    # Getting all attributes for filters
+    filters = driver.find_elements(By.CLASS_NAME, 'js-example-disabled-results')
+    for filter in filters:
+        if 'Al Ain' in filter.text:
+            Cities.append(filter.text.split('\n'))
+            continue
+        if 'Category*' in filter.text:
+            Categories.append(filter.text.split('\n'))
+            break
 except Exception as ex:
     print(ex)
 finally:
+    print(Categories)
+    print(Cities)
     driver.close()
