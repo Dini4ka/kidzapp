2dd5b76a1ae1bb2fa4ded1b6f38f938cb834c1bf
Dini4ka <boss.karelov@gmail.com>
2023-02-03 13:57:52 +0300

    косяк получается

--- bot/helper_functions.py
+++ bot/helper_functions.py
@@ -14,19 +14,19 @@ def send_file(file):
 
 
 def check_item(inside_items, outside_item):
-    keys =['address', 'age_max', 'age_min', 'all_rating_scale',
-           'all_rating_stars', 'answer_1', 'answer_2', 'answer_3',
-           'answer_4', 'book_or_no', 'calendar', 'city', 'company',
-           'contact_phone', 'description_ar', 'description_en',
-           'district_ar', 'district_en', 'emergency_phone', 'friday',
-           'header', 'image', 'map_link', 'mini_desc_ar',
-           'mini_desc_en', 'monday', 'price', 'promotes', 'question_1',
-           'question_2', 'question_3', 'question_4', 'questions_count',
-           'rating_scale', 'rating_stars', 'reason_1', 'reason_2', 'reason_3',
-           'reason_4', 'reason_5', 'reason_6', 'reviews_count', 'sale',
-           'saturday', 'schedule', 'slug', 'status', 'sunday', 'thursday',
-           'time', 'title_ar', 'title_en', 'tuesday', 'varieties_ar', 'varieties_en',
-           'video', 'views', 'website', 'wednesday', 'weekend_price', 'weekend_sale', 'x_2_bonuses']
+    keys = ['id', 'city', 'title', 'title_en', 'title_ar', 'slug', 'header',
+            'rating_stars', 'rating_scale', 'image', 'video', 'price',
+            'sale', 'weekend_price', 'weekend_sale', 'varieties_en', 'varieties_ar',
+            'age_min', 'age_max', 'time', 'monday', 'tuesday', 'wednesday', 'thursday',
+            'friday', 'saturday', 'sunday', 'district_en',
+            'district_ar', 'address', 'map_link', 'contact_phone', 'emergency_phone',
+            'mini_desc', 'mini_desc_en', 'mini_desc_ar', 'status', 'x_2_bonuses',
+            'promotes', 'reason_1', 'reason_2', 'reason_3', 'reason_4', 'reason_5',
+            'reason_6', 'question_1', 'answer_1', 'question_2', 'answer_2', 'question_3',
+            'answer_3', 'question_4', 'answer_4', 'all_rating_stars', 'all_rating_scale',
+            'reviews_count', 'questions_count', 'estimations_count', 'calendar', 'schedule',
+            'description', 'description_en', 'description_ar', 'book_or_no', 'website', 'company_id',
+            'views_id']
     for item in inside_items:
         res = True
         for key in keys:
